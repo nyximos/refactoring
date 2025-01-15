86a7b52dd85e0fb9afcb3cc0b5b2e1d9f20d59d7
nyximos <nyximos@gmail.com>
2025-01-16 00:03:24 +0900

    feat: 내역 생성 메소드 추가

--- src/main/java/com/nyximos/refactoring/ch1/Customer.java
+++ src/main/java/com/nyximos/refactoring/ch1/Customer.java
@@ -1,8 +1,9 @@
 package com.nyximos.refactoring.ch1;
 
+import java.util.Enumeration;
 import java.util.Vector;
 
-public class Customer {
+class Customer {
     private String name;
     private Vector rentals = new Vector();
 
@@ -13,5 +14,53 @@ public class Customer {
 
     public String getName() {
         return name;
+    }
+
+    public String statement() {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        Enumeration rentalElements = rentals.elements();
+        String result = "고객명: " + getName() + "\n";
+
+        while (rentalElements.hasMoreElements()) {
+            double thisAmount = 0;
+            Rental each = (Rental) rentalElements.nextElement();
+
+            // 비디오 종류별 대여료 계산
+            switch (each.getMovie().getPriceCode()) {
+                case Movie.REGULAR:
+                    thisAmount += 2;
+                    if (each.getDaysRented() > 2) {
+                        thisAmount += (each.getDaysRented() - 2) * 1.5;
+                    }
+                    break;
+                case Movie.NEW_RELEASE:
+                    thisAmount += each.getDaysRented() * 3;
+                    break;
+                case Movie.CHILDRENS:
+                    thisAmount += 1.5;
+                    if (each.getDaysRented() > 3) {
+                        thisAmount += (each.getDaysRented() - 3) * 1.5;
+                    }
+                    break;
+            }
+
+            // 적립 포인트 증가
+            frequentRenterPoints++;
+
+            // 최신물을 이틀 이상 대여시 보너스 포인트 지급
+            if (each.getMovie().getPriceCode() == Movie.NEW_RELEASE && each.getDaysRented() > 1) {
+                frequentRenterPoints++;
+            }
+
+            // 비디오 정보와 대여료
+            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
+            // 총 대여료
+            totalAmount += thisAmount;
+        }
+
+        result += "총 대여료: " + String.valueOf(totalAmount) + "\n";
+        result += "적립 포인트: " + String.valueOf(frequentRenterPoints);
+        return result;
     }
 }
